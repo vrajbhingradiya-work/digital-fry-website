1ecd0ff6b91446a9afa642fb707b1a6bcffdcd96
codewdme <lll.rg3.lll@gmail.com>
2024-01-24 18:58:06 +0530

    navbar base added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Ubuntu } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const ubuntu = Ubuntu({
   subsets: ["latin"],
@@ -21,7 +22,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={` ${ubuntu.className}`}>{children}</body>
+      <body className={` ${ubuntu.className}`}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
