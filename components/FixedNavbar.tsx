71a71d74bb6df60be74a7c7c4003016a70eb7029
codewdme <lll.rg3.lll@gmail.com>
2024-01-25 19:25:00 +0530

    adding animation to Profile part

--- components/FixedNavbar.tsx
+++ components/FixedNavbar.tsx
@@ -1,7 +1,37 @@
 "use client";
 import React from "react";
 import Link from "next/link";
+import { motion } from "framer-motion";
 const FixedNavbar = () => {
+  const Reveal = {
+    hidden: {
+      opacity: 0,
+      y: 11,
+    },
+    visible: {
+      opacity: [0, 0.3, 0.6, 0.9, 1],
+      y: [10, 8, 6, 4, 2, 0],
+      transition: {
+        delay: 0.2,
+        duration: 0.8,
+        type: "spring",
+        stiffness: 100,
+        ease: "easeInOut",
+      },
+    },
+  };
+
+  const pathVariants = {
+    hidden: {
+      opacity: 0,
+      pathLength: 0,
+    },
+    visible: {
+      opacity: 1,
+      pathLength: 1,
+      transition: { duration: 1.5, ease: "easeInOut" },
+    },
+  };
   return (
     <nav className=" flex justify-between w-full px-8 py-4 ">
       <div className="flex items-center gap-2 text-white">
@@ -13,34 +43,113 @@ const FixedNavbar = () => {
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <path
+          <motion.path
+            variants={pathVariants}
+            initial="hidden"
+            animate="visible"
             d="M7.49991 0.876892C3.84222 0.876892 0.877075 3.84204 0.877075 7.49972C0.877075 11.1574 3.84222 14.1226 7.49991 14.1226C11.1576 14.1226 14.1227 11.1574 14.1227 7.49972C14.1227 3.84204 11.1576 0.876892 7.49991 0.876892ZM7.49988 1.82689C4.36688 1.8269 1.82707 4.36672 1.82707 7.49972C1.82707 10.6327 4.36688 13.1725 7.49988 13.1726V1.82689Z"
             fill="currentColor"
             fillRule="evenodd"
             clipRule="evenodd"
           />
         </svg>
-        <p className="text-xl text-gray-600">Digital Fry</p>
+        <motion.p
+          variants={Reveal}
+          initial="hidden"
+          animate={{
+            opacity: [0, 0.3, 0.6, 0.9, 1],
+            y: [10, 8, 6, 4, 2, 0],
+            transition: {
+              duration: 0.8,
+              type: "spring",
+              stiffness: 100,
+            },
+          }}
+          className="text-xl text-gray-600"
+        >
+          Digital Fry
+        </motion.p>
       </div>
       <div className="flex items-center gap-6 ">
-        <div className="navbarItem">
+        <motion.div
+          variants={Reveal}
+          initial="hidden"
+          animate="visible"
+          className="navbarItem"
+        >
           <Link href="/">Home</Link>
-        </div>
-        <div className="navbarItem">
+        </motion.div>
+        <motion.div
+          variants={Reveal}
+          initial="hidden"
+          animate={{
+            opacity: [0, 0.3, 0.6, 0.9, 1],
+            y: [10, 8, 6, 4, 2, 0],
+            transition: {
+              delay: 0.4,
+              duration: 0.8,
+              type: "spring",
+              stiffness: 100,
+            },
+          }}
+          className="navbarItem"
+        >
           <Link href="/about">About</Link>
-        </div>
-        <div className="navbarItem">
+        </motion.div>
+        <motion.div
+          variants={Reveal}
+          initial="hidden"
+          animate={{
+            opacity: [0, 0.3, 0.6, 0.9, 1],
+            y: [10, 8, 6, 4, 2, 0],
+            transition: {
+              delay: 0.6,
+              duration: 0.8,
+              type: "spring",
+              stiffness: 100,
+            },
+          }}
+          className="navbarItem"
+        >
           <Link href="/services">Services</Link>
-        </div>
-        <div className="navbarItem">
+        </motion.div>
+        <motion.div
+          variants={Reveal}
+          initial="hidden"
+          animate={{
+            opacity: [0, 0.3, 0.6, 0.9, 1],
+            y: [10, 8, 6, 4, 2, 0],
+            transition: {
+              delay: 0.8,
+              duration: 0.8,
+              type: "spring",
+              stiffness: 100,
+            },
+          }}
+          className="navbarItem"
+        >
           <Link href="/blog">Blog</Link>
-        </div>
+        </motion.div>
       </div>
-      <div className=" ">
+      <motion.div
+        variants={Reveal}
+        initial="hidden"
+        animate={{
+          opacity: [0, 0.3, 0.6, 0.9, 1],
+          y: [10, 8, 6, 4, 2, 0],
+          transition: {
+            delay: 1,
+            duration: 0.8,
+            type: "spring",
+            stiffness: 100,
+          },
+        }}
+        className=" "
+      >
         <Link href="/contactus" className="navbarButton rounded-lg">
           Contact Us
         </Link>
-      </div>
+      </motion.div>
     </nav>
   );
 };
--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,21 +1,65 @@
 import React from "react";
 import Link from "next/link";
+import { motion } from "framer-motion";
 import FixedNavbar from "./FixedNavbar";
 
 const Profile = () => {
+  const textReveal = {
+    hidden: {
+      opacity: 0,
+      y: "5vh",
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 1.4,
+        duration: 0.5,
+        type: "tween",
+        stiffness: 300,
+      },
+    },
+  };
+
+  const textRevealChildVariants = {
+    hidden: {
+      opacity: 0,
+      y: "5vh",
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 1.6,
+        duration: 0.5,
+        type: "tween",
+        stiffness: 300,
+      },
+    },
+  };
   return (
     <div className="section mt-4 bg-black h-screen flex justify-center items-center rounded-xl relative ">
       <div className=" w-full absolute top-2 z-30 text-white">
         <FixedNavbar />
       </div>
       <div className="flex flex-col items-center justify-start gap-4 text-center ">
-        <div className=" w-4/5 sm:w-5/7 md:w-2/3  xl:w-[650px]  responsiveHeading font-black">
+        <motion.div
+          variants={textReveal}
+          initial="hidden"
+          animate="visible"
+          className=" w-4/5 sm:w-5/7 md:w-2/3  xl:w-[650px]  responsiveHeading font-black"
+        >
           Navigating the Digital Frontier with Precision and Imagination
-        </div>
-        <div className="w-3/5  sm:w-[350px]   text-xs">
+        </motion.div>
+        <motion.div
+          variants={textRevealChildVariants}
+          initial="hidden"
+          animate="visible"
+          className="w-3/5  sm:w-[350px]   text-xs"
+        >
           Maximize online impact with our creative solutions and strategic
           excellence for unparalleled digital success.
-        </div>
+        </motion.div>
       </div>
     </div>
   );
