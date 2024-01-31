32d2139a17ec8d820e4eb5dbcfc15a688dc5d74f
codewdme <lll.rg3.lll@gmail.com>
2024-02-01 00:26:03 +0530

    saving

--- components/ClientsHorizontalScroll.tsx
+++ components/ClientsHorizontalScroll.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import HorizontalScrollCarousel from "./HorizontalClientsScroll";
 
-const ClientsHorizontalScroll = ({ LogoList }: any[]) => {
+const ClientsHorizontalScroll = () => {
   return (
     <div className="section p-6  md:p-16 py-12 md:py-24  flex flex-col justify-center gap-6 ">
       <div className=" sticky top-32 text-center text-sm font-semibold text-blue-600 mb-64 ">
@@ -9,7 +9,7 @@ const ClientsHorizontalScroll = ({ LogoList }: any[]) => {
       </div>
 
       <div className="">
-        <HorizontalScrollCarousel LogoList={LogoList} />
+        <HorizontalScrollCarousel />
       </div>
     </div>
   );
