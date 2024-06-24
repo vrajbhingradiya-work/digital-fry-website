81d066ecfe38bf5bd0bee0b4e7c797a154b157eb
codewdme <lll.rg3.lll@gmail.com>
2024-06-24 16:13:09 +0530

    message update

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,11 +14,11 @@ export const metadata: Metadata = {
   title: "Digital Fry",
   description: "Digital Marketing Agency",
   icons: {
-    icon: ["/favicon.ico?v=4"],
-    apple: ["/apple-touch-icon.png?v=4"],
-    shortcut: ["/apple-touch-icon.png"],
+    icon: ["/favicon-32x32.png", "/favicon-16x16.png", "/favicon.ico?v=4"],
+    apple: "/apple-touch-icon.png?v=4",
+    shortcut: "/favicon-32x32.png",
   },
-  manifest: "site.webmanifest",
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
@@ -28,6 +28,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="icon" href="/favicon.ico?v=4" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png?v=4" />
+        <link rel="shortcut icon" href="/favicon-32x32.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body className={` ${montserrat.className}`}>
         <Navbar />
         {children}
